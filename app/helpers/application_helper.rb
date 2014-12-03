eb112c2c0c936dc481c0ff4352f073f557c35161
gsky526 <gsky526@163.com>
2014-12-03 19:30:59 +0800

    change layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+	def full_title(page_title='')
+		if page_title.empty?
+			puts "application_helper's full_title is wrong"
+		else
+			"#{page_title}"
+		end
+	end
 end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,21 +4,25 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
+    assert_select "title","主页"
   end
 
   test "should get help" do
     get :help
     assert_response :success
+    assert_select "title","帮助"
   end
 
   test "should get contact" do
     get :contact
     assert_response :success
+    assert_select "title","联系我"
   end
 
   test "should get about" do
     get :about
     assert_response :success
+    assert_select "title","关于本站"
   end
 
 end
